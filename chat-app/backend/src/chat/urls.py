b2e226c11ab5a1cbf142806484071552dc578176
Jasur <ibraimov0409@mail.ru>
2023-09-21 15:04:51 +0600

    Created views

--- chat-app/backend/src/chat/urls.py
+++ chat-app/backend/src/chat/urls.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
-from django.urls import path
-
+from django.urls import path, include
+from chatapp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('rest_framework.urls', namespace='rest_framework')),
+    # Chat message 
+    path("inbox/<user_id>/", views.MyInbox.as_view(), name="inbox"),
+    path("chat/<sender_id>/<receiver_id>/", views.GetMessages.as_view(), name="chat"),
+    path("chat/send_message/", views.SendMessage.as_view(), name="send")
 ]
--- chat-app/backend/src/chatapp/views.py
+++ chat-app/backend/src/chatapp/views.py
@@ -1,1 +1,52 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User 
+from .serializers import ChatMessageSerializer, ProfileSerializer
+from .models import ChatMessage 
+from django.db.models import Subquery, OuterRef, Q 
+from rest_framework import generics
+
+class MyInbox(generics.ListAPIView): 
+    serializer_class = ChatMessageSerializer
+
+    def get_queryset(self):
+        user_id = self.kwargs["user_id"]
+        print(user_id)
+        messages = ChatMessage.objects.filter(
+            id__in=Subquery(
+                User.objects.filter(
+                    Q(sender__receiver=user_id) | Q(receiver__sender=user_id)
+                )
+                .distinct()
+                .annotate(
+                    last_msg=Subquery(
+                        ChatMessage.objects.filter(
+                            Q(sender=OuterRef("id"), receiver=user_id) |
+                            Q(receiver=OuterRef("id"), sender=user_id)
+                        ).order_by("-id")[:1].values_list("id", flat=True)
+                    )
+                )
+                .values_list("last_msg", flat=True)
+                .order_by("-id")
+            )
+        ).order_by("-id")
+        print(messages)
+
+        return messages
+
+
+class GetMessages(generics.ListAPIView): 
+    serializer_class = ChatMessageSerializer
+
+    def get_queryset(self):
+        sender_id = self.kwargs["sender_id"]
+        receiver_id = self.kwargs["receiver_id"]
+        print(sender_id, receiver_id)
+
+        messages = ChatMessage.objects.filter(
+            sender__in = [sender_id, receiver_id],
+            receiver__in=[sender_id, receiver_id]
+        )
+        return messages
+    
+class SendMessage(generics.CreateAPIView):
+    serializer_class = ChatMessageSerializer
